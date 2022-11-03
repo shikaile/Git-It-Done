74241569ef226bd963508143f91b5111d276178a
史开乐 <csurfssecure@gmail.com>
2022-11-03 17:59:12 -0400

    working fetch request

---
+++ module-6-starter/assets/js/homepage.js
@@ -0,0 +1,16 @@
+var getUserRepos = function(user) {
+
+    var apiUrl = "https://api.github.com/users/" + user + "/repos";
+
+    fetch(apiUrl).then(function(response){
+        
+        response.json().then(function(data) {
+
+            console.log (data);
+
+        });
+    });
+
+};
+
+getUserRepos("shikaile");
